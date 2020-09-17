4c2ceba4d50747bd0307b09eda73685338e1dca5
Bastien Arcelin <barcelin@cca003.in2p3.fr>
2020-09-17 15:04:25 +0200

    update for gpu

--- scripts/tools_for_VAE/tools_for_VAE/model_gpu.py
+++ scripts/tools_for_VAE/tools_for_VAE/model_gpu.py
@@ -22,29 +22,7 @@ from tensorflow import keras
 import tensorflow as tf
 tfd = tfp.distributions
 
-
-#@tf.function
 def create_model_wo_ls(input_shape, latent_dim, hidden_dim, filters, kernels, final_dim, conv_activation=None, dense_activation=None):
-    input_layer = Input(shape=(input_shape)) 
-    #conv_activation='sigmoid'
-
-    # Encoding part
-    h = BatchNormalization()(input_layer)
-    for i in range(len(filters)):
-        h = Conv2D(filters[i], (kernels[i],kernels[i]), activation=conv_activation, padding='same')(h)
-        h = PReLU()(h)
-        h = Conv2D(filters[i], (kernels[i],kernels[i]), activation=conv_activation, padding='same', strides=(2,2))(h)
-        h = PReLU()(h)
-    h = Flatten()(h)
-    h = Dense(tfp.layers.MultivariateNormalTriL.params_size(final_dim),
-                activation=None)(h)
-    h = tfp.layers.MultivariateNormalTriL(final_dim)(h)
-
-    model = Model(input_layer,h)
-
-    return model
-
-def create_model_wo_ls_3(input_shape, latent_dim, hidden_dim, filters, kernels, final_dim, conv_activation=None, dense_activation=None):
     model = tf.keras.Sequential()
     model.add(BatchNormalization())
     for i in range(len(filters)):
@@ -56,6 +34,22 @@ def create_model_wo_ls_3(input_shape, latent_dim, hidden_dim, filters, kernels, 
     model.add(Dense(tfp.layers.MultivariateNormalTriL.params_size(final_dim), activation=None))
     model.add(tfp.layers.MultivariateNormalTriL(final_dim))
     model.build((None, 64,64,6))
-    #m = tf.keras.Sequential(h)
-
     return model
+
+# Model with tfp.experimental.nn : Reco Brian Patton
+# tfp_nn = tfp.experimental.nn
+# def create_model_wo_ls_4(input_shape, latent_dim, hidden_dim, filters, kernels, final_dim, conv_activation=None, dense_activation=None):
+#     model = tfp_nn.Sequential()
+#     model.add(BatchNormalization())
+#     for i in range(len(filters)):
+#         model.add(Conv2D(filters[i], (kernels[i],kernels[i]), activation=conv_activation, padding='same'))
+#         model.add(PReLU())
+#         model.add(Conv2D(filters[i], (kernels[i],kernels[i]), activation=conv_activation, padding='same', strides=(2,2)))
+#         model.add(PReLU())
+#     model.add(keras.layers.Flatten())
+#     model.add(Dense(tfp_nn.MultivariateNormalTriL.params_size(final_dim), activation=None))
+#     model.add(tfp_nn.MultivariateNormalTriL(final_dim))
+#     model.build((None, 64,64,6))
+#     #m = tf.keras.Sequential(h)
+
+#     return model
