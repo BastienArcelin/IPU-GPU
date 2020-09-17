851e69735d15e780ac2d08e4cdff5056ef9c826e
Bastien Arcelin <barcelin@cca003.in2p3.fr>
2020-09-17 15:04:25 +0200

    update for gpu

--- scripts/gpu/param_estimation_isolated_2.py
+++ scripts/gpu/param_estimation_isolated_2.py
@@ -25,101 +25,135 @@ import tools_for_VAE.layers as layers
 from tools_for_VAE import utils, vae_functions, generator, model_gpu
 from tensorflow.keras import backend as K
 
+#tf.debugging.set_log_device_placement(True)
+gpus = tf.config.experimental.list_physical_devices('GPU')
+print(gpus)
+for gpu in gpus:
+    tf.config.experimental.set_memory_growth(gpu, True)
+
+##### What's written below does not help increasing speed 
+#from tensorflow.keras.mixed_precision import experimental as mixed_precision
+#policy = mixed_precision.Policy('mixed_float16')
+#mixed_precision.set_policy(policy)
+#print('Compute dtype: %s' % policy.compute_dtype)
+#print('Variable dtype: %s' % policy.variable_dtype)
+
 
 ######## Parameters
 nb_of_bands = 6
 batch_size = 8
+print('The batchsize is : '+str(batch_size))
 
 input_shape = (64, 64, nb_of_bands)
 hidden_dim = 256
 latent_dim = 32
 final_dim = 3
-filters = [32, 64, 128, 256]#, 512]
-kernels = [3,3,3,3]#, 3]
+filters = [32, 64, 128, 256]
+kernels = [3,3,3,3]
 
 conv_activation = None
 dense_activation = None
 
-steps_per_epoch = 512
-validation_steps = 64
+steps_per_epoch = 1125#1100#00
+validation_steps = 125#150#50
 
 bands = [4,5,6,7,8,9]
 
 images_dir = '/sps/lsst/users/barcelin/data/TFP/GalSim_COSMOS/isolated_galaxies/centered'
 
-list_of_samples = [x for x in utils.listdir_fullpath(os.path.join(images_dir,'training')) if x.endswith('.npy')]
-list_of_samples_val = [x for x in utils.listdir_fullpath(os.path.join(images_dir,'validation')) if x.endswith('.npy')]
-list_of_samples_test = [x for x in utils.listdir_fullpath(os.path.join(images_dir,'test')) if x.endswith('.npy')]
-print(list_of_samples_test)
+# list_of_samples = [x for x in utils.listdir_fullpath(os.path.join(images_dir,'test')) if x.endswith('.npy')]
+# list_of_samples_val = [x for x in utils.listdir_fullpath(os.path.join(images_dir,'test')) if x.endswith('.npy')]
+# list_of_samples_test = [x for x in utils.listdir_fullpath(os.path.join(images_dir,'test')) if x.endswith('.npy')]
+# print(list_of_samples_test)
 
+data = np.load('/sps/lsst/users/barcelin/data/TFP/GalSim_COSMOS/isolated_galaxies/centered/test/galaxies_isolated_20191024_0_images.npy', mmap_mode = 'c')
+data_label = pd.read_csv('/sps/lsst/users/barcelin/data/TFP/GalSim_COSMOS/isolated_galaxies/centered/test/galaxies_isolated_20191024_0_data.csv')
+#training_data = np.zeros((9000,2))
+x_train = tf.transpose(data[:9000,1], perm= [0,2,3,1])[:,:,:,4:]
+y_train = np.zeros((9000,3))
+y_train[:,0] = data_label[:9000]['e1']
+y_train[:,1] = data_label[:9000]['e2']
+y_train[:,2] = data_label[:9000]['redshift']
+y_train = tf.convert_to_tensor(y_train)
+ds_train = tf.data.Dataset.from_tensor_slices((np.expand_dims(x_train, axis = 1), np.expand_dims(y_train,axis = 1)))
+
+x_val = tf.transpose(data[9000:,1], perm= [0,2,3,1])[:,:,:,4:]
+y_val = np.zeros((1000,3))
+y_val[:,0] = data_label[9000:]['e1']
+y_val[:,1] = data_label[9000:]['e2']
+y_val[:,2] = data_label[9000:]['redshift']
+y_val = tf.convert_to_tensor(y_val)
+ds_val = tf.data.Dataset.from_tensor_slices((np.expand_dims(x_val, axis = 1), np.expand_dims(y_val, axis = 1)))
 
 #With generator (unchanged)
-training_generator = generator.BatchGenerator(bands, list_of_samples, total_sample_size=None,
-                                    batch_size=batch_size, 
-                                    trainval_or_test='training',
-                                    do_norm=False,
-                                    denorm = False,
-                                    list_of_weights_e=None)
+# training_generator = generator.BatchGenerator(bands, list_of_samples, total_sample_size=None,
+#                                     batch_size=batch_size, 
+#                                     trainval_or_test='training',
+#                                     do_norm=False,
+#                                     denorm = False,
+#                                     list_of_weights_e=None)
 
-validation_generator = generator.BatchGenerator(bands, list_of_samples_val, total_sample_size=None,
-                                    batch_size=batch_size, 
-                                    trainval_or_test='validation',
-                                    do_norm=False,
-                                    denorm = False,
-                                    list_of_weights_e=None)
+# validation_generator = generator.BatchGenerator(bands, list_of_samples_val, total_sample_size=None,
+#                                     batch_size=batch_size, 
+#                                     trainval_or_test='validation',
+#                                     do_norm=False,
+#                                     denorm = False,
+#                                     list_of_weights_e=None)
 
-test_generator = generator.BatchGenerator(bands, list_of_samples_test, total_sample_size=None,
-                                    batch_size=batch_size, 
-                                    trainval_or_test='test',
-                                    do_norm=False,
-                                    denorm = False,
-                                    list_of_weights_e=None)
+# test_generator = generator.BatchGenerator(bands, list_of_samples_test, total_sample_size=None,
+#                                     batch_size=batch_size, 
+#                                     trainval_or_test='test',
+#                                     do_norm=False,
+#                                     denorm = False,
+#                                     list_of_weights_e=None)
 
 
 # NEW: Wrap the generator.BatchGenerator objects in a generator-style function
 # which we can then pass to tf.data.Dataset.from_generator()
-# (One per train/val/test dataset at the moment, but could be refactored for neatness!)
-def training_batch_generator():
-    multi_enqueuer = keras.utils.OrderedEnqueuer(training_generator,
-                                                use_multiprocessing=False)
-    multi_enqueuer.start(workers=10, max_queue_size=10)
-    while True:
-        batch_x, batch_y = next(multi_enqueuer.get())
-        yield batch_x, batch_y
+# # (One per train/val/test dataset at the moment, but could be refactored for neatness!)
+# def training_batch_generator():
+#     multi_enqueuer = keras.utils.OrderedEnqueuer(training_generator,
+#                                                 use_multiprocessing=False)
+#     multi_enqueuer.start(workers=10, max_queue_size=10)
+#     while True:
+#         batch_x, batch_y = next(multi_enqueuer.get())
+#         yield batch_x, batch_y
 
-def validation_batch_generator():
-    multi_enqueuer = keras.utils.OrderedEnqueuer(validation_generator,
-                                                    use_multiprocessing=False)
-    multi_enqueuer.start(workers=10, max_queue_size=10)
-    while True:
-        batch_x, batch_y = next(multi_enqueuer.get())
-        yield batch_x, batch_y
+# def validation_batch_generator():
+#     multi_enqueuer = keras.utils.OrderedEnqueuer(validation_generator,
+#                                                     use_multiprocessing=False)
+#     multi_enqueuer.start(workers=10, max_queue_size=10)
+#     while True:
+#         batch_x, batch_y = next(multi_enqueuer.get())
+#         yield batch_x, batch_y
 
-def test_batch_generator():
-    multi_enqueuer = keras.utils.OrderedEnqueuer(test_generator,
-                                                    use_multiprocessing=False)
-    multi_enqueuer.start(workers=10, max_queue_size=10)
-    while True:
-        batch_x, batch_y = next(multi_enqueuer.get())
-        yield batch_x, batch_y
+# def test_batch_generator():
+#     multi_enqueuer = keras.utils.OrderedEnqueuer(test_generator,
+#                                                     use_multiprocessing=False)
+#     multi_enqueuer.start(workers=10, max_queue_size=10)
+#     while True:
+#         batch_x, batch_y = next(multi_enqueuer.get())
+#         yield batch_x, batch_y
 
-# Recommended to specify the expected output shapes and types here
-output_types = (tf.float32, tf.float32)
-output_shapes = (tf.TensorShape([batch_size, 64, 64, nb_of_bands]),
-                    tf.TensorShape([batch_size, 3]))
+# # Recommended to specify the expected output shapes and types here
+# output_types = (tf.float32, tf.float32)
+# output_shapes = (tf.TensorShape([batch_size, 64, 64, nb_of_bands]),
+#                     tf.TensorShape([batch_size, 3]))
 
-training_ds = tf.data.Dataset.from_generator(training_batch_generator,
-                                                output_types=output_types,
-                                                output_shapes=output_shapes).repeat()
+# training_ds = tf.data.Dataset.from_generator(training_batch_generator,
+#                                                 output_types=output_types,
+#                                                 output_shapes=output_shapes).repeat()
 
-validation_ds = tf.data.Dataset.from_generator(validation_batch_generator,
-                                                output_types=output_types,
-                                                output_shapes=output_shapes).repeat()
+# validation_ds = tf.data.Dataset.from_generator(validation_batch_generator,
+#                                                 output_types=output_types,
+#                                                 output_shapes=output_shapes).repeat()
 
-test_ds = tf.data.Dataset.from_generator(test_batch_generator,
-                                            output_types=output_types,
-                                            output_shapes=output_shapes).repeat()
+# test_ds = tf.data.Dataset.from_generator(test_batch_generator,
+#                                             output_types=output_types,
+#                                             output_shapes=output_shapes).repeat()
 
+# strategy = tf.distribute.OneDeviceStrategy(device="/gpu:0")
+# with strategy.scope():
 
 
 #### Model definition
@@ -129,7 +163,7 @@ if model_choice == 'ls':
     net = model_gpu.create_model(input_shape, latent_dim, hidden_dim, filters, kernels, final_dim, conv_activation=None, dense_activation=None)
 # Without latent space
 if model_choice == 'wo_ls':
-    net = model_gpu.create_model_wo_ls_3(input_shape, latent_dim, hidden_dim, filters, kernels, final_dim, conv_activation=None, dense_activation=None)
+    net = model_gpu.create_model_wo_ls(input_shape, latent_dim, hidden_dim, filters, kernels, final_dim, conv_activation=None, dense_activation=None)
 # Full probabilistic model
 if model_choice == 'full_prob':
     net = model_gpu.create_model_full_prob(input_shape, latent_dim, hidden_dim, filters, kernels, final_dim, conv_activation=None, dense_activation=None)
@@ -148,36 +182,21 @@ net.compile(optimizer=tf.optimizers.Adam(learning_rate=1e-3),
             loss="mean_squared_error")
 
 
-#loading_path = '/home/astrodeep/bastien/weights/'
-#latest = tf.train.latest_checkpoint(loading_path)
-#net.load_weights(latest)
-
-
 ######## Train the network
-# hist = net.fit(training_ds, steps_per_epoch=2, epochs=1, verbose = 1)#576
-hist = net.fit(training_ds, epochs=10,
+hist = net.fit(x_train, y_train, batch_size = batch_size, epochs=10,#x_train, y_train#training_ds
                     steps_per_epoch=steps_per_epoch,
-                    verbose=1,
+                    verbose=2,
                     shuffle=True,
-                    validation_data=validation_ds,
+                    validation_data=(x_val,y_val),#(x_val,y_val),#validation_ds,
                     validation_steps=validation_steps)#,
-                    #workers=0,#4 
+                    #workers=0, 
                     #use_multiprocessing = True)
 
 
 net.summary()
 
-    # hist = net.fit(training_data, training_labels, epochs=50, # training
-    #                     steps_per_epoch=steps_per_epoch,#128
-    #                     verbose=1,
-    #                     shuffle=True,
-    #                     validation_data=(validation_data,validation_labels), # validation
-    #                     validation_steps=validation_steps)#,#16
-    #                     #workers=0,#4 
-    #                     #use_multiprocessing = True)
-
-saving_path = '/home/astrodeep/bastien/weights/'#test_5
-net.save_weights('test')
+# saving_path = '/home/astrodeep/bastien/weights/'#test_5
+# net.save_weights('test')
 
 
 #### Plots
@@ -192,74 +211,63 @@ net.save_weights('test')
 #new_net.load_weights(latest)
 
 
-net.load_weights('test')#loading_path)
-print('weights loaded')
+# net.load_weights('test')#loading_path)
+# print('weights loaded')
 
 
 
-training_data, training_labels  = next(iter(test_ds))
-print(training_data.numpy().shape)
+# training_data, training_labels  = next(iter(test_ds))
+# print(training_data.numpy().shape)
+
+# ds = tf.data.Dataset.from_tensor_slices((tf.cast(training_data, tf.float32)))
+
+# out = net.predict(ds.batch(8, drop_remainder=True))#dataset # ,steps_per_epoch = 1)#training_data     , steps = 1,steps_per_run=8
+
+# fig = plt.figure()
+# sns.distplot(out.mean().numpy()[:,0], bins = 20)
+# sns.distplot(training_labels[:,0], bins = 20)
+# fig.savefig('test_distrib_e1.png')
 
 
-#ds = tf.data.Dataset.from_tensor_slices([training_data]).batch(8, drop_remainder=True)
-
-# output_types = (tf.float32, tf.float32)
-# output_shapes = (tf.TensorShape([batch_size, 64, 64, nb_of_bands]),
-#                     tf.TensorShape([batch_size, 3]))
-# ds = tf.data.Dataset.from_tensor_generator(test_batch_generator,
-#                                              output_types=output_types,
-#                                              output_shapes=output_shapes)
-#out = net.evaluate(test_ds, steps = 1)#.batch(8, drop_remainder=True)
-#print('evaluate ok: '+str(out))
-ds = tf.data.Dataset.from_tensor_slices((tf.cast(training_data, tf.float32)))
-
-out = net.predict(ds.batch(8, drop_remainder=True))#dataset # ,steps_per_epoch = 1)#training_data     , steps = 1,steps_per_run=8
-
-fig = plt.figure()
-sns.distplot(out.mean().numpy()[:,0], bins = 20)
-sns.distplot(training_labels[:,0], bins = 20)
-fig.savefig('test_distrib_e1.png')
+# fig = plt.figure()
+# sns.distplot(out.mean().numpy()[:,1], bins = 20)
+# sns.distplot(training_labels[:,1], bins = 20)
+# fig.savefig('test_distrib_e2.png')
 
 
-fig = plt.figure()
-sns.distplot(out.mean().numpy()[:,1], bins = 20)
-sns.distplot(training_labels[:,1], bins = 20)
-fig.savefig('test_distrib_e2.png')
+# fig = plt.figure()
+# sns.distplot(out.mean().numpy()[:,2], bins = 20)
+# sns.distplot(training_labels[:,2], bins = 20)
+# fig.savefig('test_distrib_e3.png')
 
+# fig, axes = plt.subplots(1, 3, figsize=(15, 5))
 
-fig = plt.figure()
-sns.distplot(out.mean().numpy()[:,2], bins = 20)
-sns.distplot(training_labels[:,2], bins = 20)
-fig.savefig('test_distrib_e3.png')
+# axes[0].plot(training_labels[:,0], out.mean().numpy()[:,0], '.', label = 'mean')
+# axes[0].plot(training_labels[:,0], out.mean().numpy()[:,0]+ 2*out.stddev().numpy()[:,0], '+', label = 'mean + 2stddev')
+# axes[0].plot(training_labels[:,0], out.mean().numpy()[:,0]- 2*out.stddev().numpy()[:,0], '+', label = 'mean - 2stddev')
+# x = np.linspace(-1,1)
+# axes[0].plot(x, x)
+# axes[0].legend()
+# axes[0].set_ylim(-1,1)
+# axes[0].set_title('$e1$')
 
-fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+# axes[1].plot(training_labels[:,1], out.mean().numpy()[:,1], '.', label = 'mean')
+# axes[1].plot(training_labels[:,1], out.mean().numpy()[:,1]+ 2*out.stddev().numpy()[:,1], '+', label = 'mean + 2stddev')
+# axes[1].plot(training_labels[:,1], out.mean().numpy()[:,1]- 2*out.stddev().numpy()[:,1], '+', label = 'mean - 2stddev')
+# x = np.linspace(-1,1)
+# axes[1].plot(x, x)
+# axes[1].legend()
+# axes[1].set_ylim(-1,1)
+# axes[1].set_title('$e2$')
 
-axes[0].plot(training_labels[:,0], out.mean().numpy()[:,0], '.', label = 'mean')
-axes[0].plot(training_labels[:,0], out.mean().numpy()[:,0]+ 2*out.stddev().numpy()[:,0], '+', label = 'mean + 2stddev')
-axes[0].plot(training_labels[:,0], out.mean().numpy()[:,0]- 2*out.stddev().numpy()[:,0], '+', label = 'mean - 2stddev')
-x = np.linspace(-1,1)
-axes[0].plot(x, x)
-axes[0].legend()
-axes[0].set_ylim(-1,1)
-axes[0].set_title('$e1$')
+# axes[2].plot(training_labels[:,2], out.mean().numpy()[:,2], '.', label = 'mean')
+# axes[2].plot(training_labels[:,2], out.mean().numpy()[:,2]+ 2*out.stddev().numpy()[:,2], '+', label = 'mean + 2stddev')
+# axes[2].plot(training_labels[:,2], out.mean().numpy()[:,2]- 2*out.stddev().numpy()[:,2], '+', label = 'mean - 2stddev')
+# x = np.linspace(0,4)
+# axes[2].plot(x, x)
+# axes[2].legend()
+# axes[2].set_ylim(-1,5.5)
+# axes[2].set_title('$z$')
 
-axes[1].plot(training_labels[:,1], out.mean().numpy()[:,1], '.', label = 'mean')
-axes[1].plot(training_labels[:,1], out.mean().numpy()[:,1]+ 2*out.stddev().numpy()[:,1], '+', label = 'mean + 2stddev')
-axes[1].plot(training_labels[:,1], out.mean().numpy()[:,1]- 2*out.stddev().numpy()[:,1], '+', label = 'mean - 2stddev')
-x = np.linspace(-1,1)
-axes[1].plot(x, x)
-axes[1].legend()
-axes[1].set_ylim(-1,1)
-axes[1].set_title('$e2$')
-
-axes[2].plot(training_labels[:,2], out.mean().numpy()[:,2], '.', label = 'mean')
-axes[2].plot(training_labels[:,2], out.mean().numpy()[:,2]+ 2*out.stddev().numpy()[:,2], '+', label = 'mean + 2stddev')
-axes[2].plot(training_labels[:,2], out.mean().numpy()[:,2]- 2*out.stddev().numpy()[:,2], '+', label = 'mean - 2stddev')
-x = np.linspace(0,4)
-axes[2].plot(x, x)
-axes[2].legend()
-axes[2].set_ylim(-1,5.5)
-axes[2].set_title('$z$')
-
-fig.savefig('test_train.png')
+# fig.savefig('test_train.png')
 
