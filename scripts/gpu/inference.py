250e900bb5ced442f97b617af04b6bd3d9febab2
Bastien Arcelin <barcelin@ccwgige016.in2p3.fr>
2020-09-24 18:01:09 +0200

    update and clean inference.py

--- scripts/gpu/inference.py
+++ scripts/gpu/inference.py
@@ -5,27 +5,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
-import collections
-from importlib import reload
 import time
-
-# Tensorflow
-import tensorflow
 import tensorflow as tf
-import tensorflow_probability as tfp
-from tensorflow import keras
-from tensorflow.keras.layers import Input, Dense, Lambda, Layer, Add, Multiply, Reshape, Flatten, BatchNormalization
-from tensorflow.keras.models import Model, Sequential
-from tensorflow.keras.layers import Conv2D, Input, Dense, Dropout, MaxPool2D, Flatten,  Reshape, UpSampling2D, Cropping2D, Conv2DTranspose, PReLU, Concatenate, Lambda, BatchNormalization, concatenate, LeakyReLU
 
-tfd = tfp.distributions
-
+### Needed files
 sys.path.insert(0,'../../scripts/tools_for_VAE/')
-import tools_for_VAE.layers as layers
-from tools_for_VAE import utils, vae_functions, generator, model_gpu
-from tensorflow.keras import backend as K
+from tools_for_VAE import utils, model_gpu
 
-#tf.debugging.set_log_device_placement(True)
 gpus = tf.config.experimental.list_physical_devices('GPU')
 print(gpus)
 for gpu in gpus:
@@ -46,22 +32,18 @@ kernels = [3,3,3,3]
 
 conv_activation = None
 dense_activation = None
-
-steps_per_epoch = 1125#1100#00
-validation_steps = 125#150#50
-
 bands = [4,5,6,7,8,9]
 
+### Load data
 images_dir = '/sps/lsst/users/barcelin/data/TFP/GalSim_COSMOS/isolated_galaxies/centered'
 
-# list_of_samples = [x for x in utils.listdir_fullpath(os.path.join(images_dir,'test')) if x.endswith('.npy')]
-# list_of_samples_val = [x for x in utils.listdir_fullpath(os.path.join(images_dir,'test')) if x.endswith('.npy')]
-# list_of_samples_test = [x for x in utils.listdir_fullpath(os.path.join(images_dir,'test')) if x.endswith('.npy')]
-# print(list_of_samples_test)
+list_of_samples_test = [x for x in utils.listdir_fullpath(os.path.join(images_dir,'test')) if x.endswith('.npy')]
+list_of_samples_test_labels = [x for x in utils.listdir_fullpath(os.path.join(images_dir,'test')) if x.endswith('.csv')]
 
-data = np.load('/sps/lsst/users/barcelin/data/TFP/GalSim_COSMOS/isolated_galaxies/centered/test/galaxies_isolated_20191024_0_images.npy', mmap_mode = 'c')
-data_label = pd.read_csv('/sps/lsst/users/barcelin/data/TFP/GalSim_COSMOS/isolated_galaxies/centered/test/galaxies_isolated_20191024_0_data.csv')
-#training_data = np.zeros((9000,2))
+data = np.load(list_of_samples_test[0], mmap_mode = 'c')
+data_label = pd.read_csv(list_of_samples_test_labels[0])
+
+### Define samples
 x_train = tf.transpose(data[:9000,1], perm= [0,2,3,1])[:,:,:,4:]
 y_train = np.zeros((9000,3))
 y_train[:,0] = data_label[:9000]['e1']
@@ -79,13 +61,10 @@ y_val = tf.convert_to_tensor(y_val)
 ds_val = tf.data.Dataset.from_tensor_slices((np.expand_dims(x_val, axis = 1), np.expand_dims(y_val, axis = 1)))
 
 #### Model definition
-model_choice = 'wo_ls'
-# With latent space
-if model_choice == 'ls':
-    net = model_gpu.create_model(input_shape, latent_dim, hidden_dim, filters, kernels, final_dim, conv_activation=None, dense_activation=None)
-# Without latent space
-if model_choice == 'wo_ls':
-    net = model_gpu.create_model_wo_ls(input_shape, latent_dim, hidden_dim, filters, kernels, final_dim, conv_activation=None, dense_activation=None)
+model_choice = 'det'
+# Fully deterministic model
+if model_choice == 'det':
+    net = model_gpu.create_model_det(input_shape, latent_dim, hidden_dim, filters, kernels, final_dim, conv_activation=None, dense_activation=None)
 # Full probabilistic model
 if model_choice == 'full_prob':
     net = model_gpu.create_model_full_prob(input_shape, latent_dim, hidden_dim, filters, kernels, final_dim, conv_activation=None, dense_activation=None)
@@ -105,20 +84,14 @@ net.compile(optimizer=tf.optimizers.Adam(learning_rate=1e-3),
 
 net.summary()
 
-
+### Load model
 loading_path = '/sps/lsst/users/barcelin/weights/gpu_benchmark/'
 latest = tf.train.latest_checkpoint(loading_path)
 net.load_weights(latest)
 
 print('weights loaded')
 
-
-
-# training_data, training_labels  = next(iter(test_ds))
-# print(training_data.numpy().shape)
-
-# ds = tf.data.Dataset.from_tensor_slices((tf.cast(training_data, tf.float32)))
-
+### Do inference
 t_1 = time.time()
 out = []
 for i in range (len(x_val)):
@@ -129,6 +102,7 @@ t_2 = time.time()
 print('prediction done in: '+str(t_2-t_1)+' seconds')
 print(out.shape)
 
+### Plot results
 fig = plt.figure()
 sns.distplot(out.mean().numpy()[:,0], bins = 20)
 sns.distplot(y_val[:,0], bins = 20)
